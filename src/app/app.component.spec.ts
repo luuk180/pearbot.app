a5ac429b81e04c8508191a8371ee2ddc20b20248
Luuk Volkering <luuk.volkering@gmail.com>
2020-11-22 22:58:08 +0100

    Added functionality to send messages!

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LayoutModule } from '@angular/cdk/layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -21,8 +24,12 @@ describe('AppComponent', () => {
         MatIconModule,
         MatListModule,
         MatSidenavModule,
+        FormsModule,
+        ReactiveFormsModule,
         MatToolbarModule,
-        RouterTestingModule
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatInputModule,
       ],
       declarations: [
         AppComponent
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { FormBuilder} from '@angular/forms';
 
 export interface Item { name: string; }
 
@@ -10,14 +11,33 @@ export interface Item { name: string; }
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  messageForm;
+
   private itemsCollection: AngularFirestoreCollection<Item>;
   entries: Observable<Item[]>;
-  constructor(private afs: AngularFirestore) {
+  constructor(
+    private afs: AngularFirestore,
+    private formBuilder: FormBuilder,
+  ) {
     this.itemsCollection = afs.collection<Item>('messages');
     this.entries = this.itemsCollection.valueChanges(['added', 'removed']);
+
+    this.messageForm = this.formBuilder.group({
+      message: ''
+    })
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(messageSent) {
+    console.log(messageSent);
+    var timestamp = new Date();
+    this.itemsCollection.add({
+      message: messageSent.message,
+      time: timestamp
+    });
+    this.messageForm.reset();
   }
 
 }
